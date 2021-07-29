409ec08c93422c66f2e743182acd376f7574e0d4
jacques.weideman <jacques.weideman@hotmail.com>
2021-07-29 20:15:28 +0200

    rfgst

--- src/App.js
+++ src/App.js
@@ -8,12 +8,62 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      weather: []
+      weather: [],
+      city: '',
+      temp: ''
     }
-  }
+  }  
 
   componentDidMount() {
-    fetch("")
+    const api = process.env.WEATHER_API_KEY;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=${api}&units=metric`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            weather: result.weather,
+            city: result.name,
+            temp: result.main.temp
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const {error, isLoaded, weather} = this.state;
+
+    if (error){
+      return(
+        <div>
+          Error : {error.message}
+        </div>
+      );
+    }else if (!isLoaded){
+      return (
+        <div>
+          Loading...
+        </div>
+      );
+    }else{
+      return(
+        <div>
+          <h1>{this.state.city}</h1>
+          {weather.map(weather => (
+            <div>              
+              <h2>{weather.description}</h2>
+            </div>
+          ))}
+          <h2>{this.state.temp}</h2>
+        </div>
+      )
+    }
   }
 }
 
